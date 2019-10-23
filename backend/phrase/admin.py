03a48ad59418d5132b26817f8cd9421efd2f5b5a
Eduardo Lopez <eduardolopezlainez2001@gmail.com>
2019-10-23 00:12:26 -0600

    Phrase-Model Fixed

--- backend/phrase/admin.py
+++ backend/phrase/admin.py
@@ -4,6 +4,6 @@ from .models import Phrase
 # Register your models here.
 
 class PhraseAdmin(admin.ModelAdmin):
-    list_display = ('user','title','phrase','date')
+    list_display = ('user','title','description','date')
 
 admin.site.register(Phrase,PhraseAdmin)    
--- backend/phrase/models/phrase.py
+++ backend/phrase/models/phrase.py
@@ -7,7 +7,7 @@ class Phrase(models.Model):
     id = models.AutoField(primary_key = True)
     user = models.ForeignKey(User , on_delete = models.CASCADE)
     title = models.CharField(max_length = 30)
-    phrase = models.TextField(max_length = 100)
+    description = models.TextField(max_length = 100)
     date = models.DateField()
 
     def __str__(self):
